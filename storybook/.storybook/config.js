50fabdcd540f8d39d15af3bc563a0eac1cb7d106
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-04 00:25:52 -0700

    Visual regression testing

--- storybook/.storybook/config.js
+++ storybook/.storybook/config.js
@@ -1,6 +1,6 @@
 import { configure } from '@storybook/react';
 import requireContext from 'require-context.macro';
-
+import 'storybook-chromatic';
 import '../src/index.css';
 
 const req = requireContext('../src/components', true, /\.stories\.js$/);
