9f0024b556841bfad56e51e18963857eb3fc70f6
Trevor Fehrman <trevorfehrman@gmail.com>
2019-04-04 00:46:55 -0700

    knobs

--- storybook/src/components/Task.stories.js
+++ storybook/src/components/Task.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withKnobs, object } from '@storybook/addon-knobs/react';
 
 import Task from './Task';
 
@@ -8,15 +9,18 @@ export const task = {
   id: '1',
   title: 'Test Task',
   state: 'TASK_INBOX',
-  updatedAt: new Date(2018, 0, 1, 9, 0),
+  updatedAt: new Date(2018, 0, 1, 9, 0)
 };
 
 export const actions = {
   onPinTask: action('onPinTask'),
-  onArchiveTask: action('onArchiveTask'),
+  onArchiveTask: action('onArchiveTask')
 };
 
 storiesOf('Task', module)
-  .add('default', () => <Task task={task} {...actions} />)
+  .addDecorator(withKnobs)
+  .add('default', () => {
+    return <Task task={object('task', { ...task })} {...actions} />;
+  })
   .add('pinned', () => <Task task={{ ...task, state: 'TASK_PINNED' }} {...actions} />)
   .add('archived', () => <Task task={{ ...task, state: 'TASK_ARCHIVED' }} {...actions} />);
